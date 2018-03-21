da4bfcf0a43934ceb6b060a3fec7bf72068ca935
Spencer Corkran <corkransp@gmail.com>
2018-03-21 13:31:36 -0400

    Initial commit
    Player, Launch, Ground

---
+++ Paper Planes Deluxe - Overdrive/Assets/Launch.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Launch : MonoBehaviour {
+
+	Rigidbody2D rb;
+	bool isAirborne;
+	bool track;
+	bool isLaunching;
+
+	public Text currentDistance;
+	public Text currentHeight;
+	public Text maxHeight;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+		isLaunching = false;
+	}
+
+	public void Throw(float force)
+	{
+		rb.simulated = true;
+		rb.AddRelativeForce(new Vector2(force, 0), ForceMode2D.Impulse);
+		isAirborne = true;
+		track = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Ragdoll
+		if(isAirborne)
+		{
+			Vector2 v = rb.velocity;
+			float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+	 		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	 	}
+
+	 	//UI
+	 	if(track)
+	 	{
+	 		currentDistance.text = "Distance: " + transform.position.x.ToString();
+	 		currentHeight.text = "Current Height: " + transform.position.y.ToString();
+	 		if(transform.position.y > float.Parse(maxHeight.text))
+	 			maxHeight.text = transform.position.y.ToString();
+	 	}
+
+	 	//Control
+	 	if(Input.GetMouseButton(0))
+	 	{
+
+	 	}
+	 	if(Input.GetMouseButtonUp(0) && isLaunching)
+	 	{
+
+	 	}
+	 	//Debug
+	 	if((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+	 	 && isLaunching)
+	 	{
+	 		Throw(25);
+	 	}
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.tag == "Ground")
+		{
+			isAirborne = false;
+		}
+	}
+}
