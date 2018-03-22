fee2fbdcd2c2fcae6fe9fe10383935de3902d6f7
Spencer Corkran <corkransp@gmail.com>
2018-03-22 10:48:04 -0400

    Store toggle

--- Paper Planes Deluxe - Overdrive/Assets/Store.cs
+++ Paper Planes Deluxe - Overdrive/Assets/Store.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class Store : MonoBehaviour {
 
+	private CanvasGroup storeGroup;
+	private bool isVisible;
+
 	// Use this for initialization
 	void Start () {
-		
+		storeGroup = gameObject.GetComponent<CanvasGroup>();
+		isVisible = false;
+	}
+
+	public void Toggle()
+	{
+		isVisible = !isVisible;
+		if(isVisible)
+		{
+			storeGroup.alpha = 1;
+			storeGroup.interactable = true;
+		}
+		else
+		{
+			storeGroup.alpha = 0;
+			storeGroup.interactable = false;
+		}
 	}
 	
 	// Update is called once per frame
